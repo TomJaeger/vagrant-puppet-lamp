e5a0d0effe10e0810abb45d2df3f32faea9cafc9
Vincent Van Driessche <vncntvandriessche@gmail.com>
2013-12-05 16:45:57 +0100

    Adds a restart of the apache service

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -14,7 +14,6 @@ class php {
     "php5-mcrypt",
     "php5-gd",
     "php5-curl",
-    "libapache2-mod-php5"
   ]
 
   package { $packages:
@@ -24,4 +23,9 @@ class php {
   exec { "sed -i 's|#|//|' /etc/php5/cli/conf.d/mcrypt.ini":
       require => Package['php5'],
   }
+
+  package { 'libapache2-mod-php5':
+    notify => Service['apache2']
+  }
+
 }

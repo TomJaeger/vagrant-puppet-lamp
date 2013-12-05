3af1a3102543b04297e1b6a688b1801d1150adc0
Vincent Van Driessche <vncntvandriessche@gmail.com>
2013-12-05 16:22:40 +0100

    Fixes indenting and trailing whitespaces

--- puppet/manifests/sdi03.pp
+++ puppet/manifests/sdi03.pp
@@ -1,13 +1,10 @@
 # Default path
-Exec 
-{
+Exec {
   path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 }
 
-exec 
-{ 
-    'apt-get update':
-        command => '/usr/bin/apt-get update'
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
 }
 
 include bootstrap
